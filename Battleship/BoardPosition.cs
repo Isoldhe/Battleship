5ba399db07ef67728c5fd62e41c637ac9fd9a5c7
Linda de Haan <linda88haan@gmail.com>
2019-03-12 20:44:38 +0100

    StringComparer stuff

--- Battleship/BoardPosition.cs
+++ Battleship/BoardPosition.cs
@@ -14,17 +14,16 @@ namespace Battleship
         {
             Dictionary = new Dictionary<string, char>(StringComparer.OrdinalIgnoreCase)
             {
-                { "A", 'a' }
-                //{ "a1", 0 },
-                //{ "B", 1 },
-                //{ "C", 2 },
-                //{ "D", 3 },
-                //{ "E", 4 },
-                //{ "F", 5 },
-                //{ "G", 6 },
-                //{ "H", 7 },
-                //{ "I", 8 },
-                //{ "J", 9 }
+                { "a", '0' },
+                { "b", '1' },
+                { "c", '2' },
+                { "d", '3' },
+                { "e", '4' },
+                { "f", '5' },
+                { "g", '6' },
+                { "h", '7' },
+                { "i", '8' },
+                { "j", '9' }
             };
         }
     }
