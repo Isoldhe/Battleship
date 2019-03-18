5683dc760e8ced86d074b4aab9da655dd49e159b
Atrix <nfvanipenburg@gmail.com>
2019-03-18 22:52:15 +0100

    Added logic for selecting a ship type.

--- Battleship/DisplayElements/ShipTypeSelector.cs
+++ Battleship/DisplayElements/ShipTypeSelector.cs
@@ -9,37 +9,85 @@ namespace Battleship.DisplayElements
 {
     public class ShipTypeSelector : DisplayElement
     {
+        private readonly ShipType[] _shipTypes = Enum.GetValues(typeof(ShipType)).OfType<ShipType>().ToArray();
+        private ShipType? _selectedShipType = null;
+
         public ShipTypeSelector(int width, int height) : base(width, height)
         {
-            //TODO logic for selecting a ship type to place
-            //TODO hittesting for mouse input
-            //TODO color change on ship type selection and when it has been placed
+            //TODO color change when ship type has been placed
             //TODO? use same ships list as BattleField as backing store for which ships
             // have been placed and eventually later ship damage indication?
 
             FillBuffer();
         }
 
+        public ShipType? SelectedShipType
+        {
+            get => _selectedShipType;
+            private set
+            {
+                UnHighlightSelectedType();
+                _selectedShipType = value;
+                HighlightSelectedType();
+            }
+        }
+
+        public void SelectShipType(short x, short y)
+        {
+            var row = y - Top;
+            if (row < _shipTypes.Length)
+            {
+                SelectedShipType = _shipTypes[row];
+            }
+        }
+
+        public void SelectShipType(string input)
+        {
+            var types = _shipTypes.Where(shipType => shipType.ToString().StartsWith(input, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (types.Any())
+            {
+                SelectedShipType = types.First();
+            }
+        }
+
         private void FillBuffer()
         {
-            var shipTypes = Enum.GetValues(typeof(ShipType)).OfType<ShipType>().ToArray();
             for (int row = 0; row < Height; row++)
             {
                 string shipString = "";
-                if (row < shipTypes.Length)
+                if (row < _shipTypes.Length)
                 {
-                    var ship = new Ship(shipTypes[row], 0, 0, Orientation.Horizontal);
-                    shipString = $"[{ship.Name[0]}] {shipTypes[row]} Size: {ship.Size}";
+                    var ship = new Ship(_shipTypes[row], 0, 0, Orientation.Horizontal);
+                    shipString = $"[{ship.Name[0]}] {_shipTypes[row]} Size: {ship.Size}";
                 }
                 for (int column = 0; column < Width; column++)
                 {
                     if (column < shipString.Length)
                     {
                         Buffer[row][column].Character = column < shipString.Length ? shipString[column] : ' ';
-                        Buffer[row][column].Attributes &= ~CharAttributes.FOREGROUND_INTENSITY;
                     }
+                    Buffer[row][column].Attributes &= ~CharAttributes.FOREGROUND_INTENSITY;
                 }
             }
+        }
+
+        private void UnHighlightSelectedType()
+        {
+            if (SelectedShipType == null)
+                return;
+
+            for (int column = 0; column < Width; column++)
+                Buffer[(int)SelectedShipType][column].Attributes &= ~CharAttributes.FOREGROUND_INTENSITY;
+        }
+
+        private void HighlightSelectedType()
+        {
+            if (SelectedShipType == null)
+                return;
+
+            for (int column = 0; column < Width; column++)
+                Buffer[(int)SelectedShipType][column].Attributes |= CharAttributes.FOREGROUND_INTENSITY;
         }
     }
 }
